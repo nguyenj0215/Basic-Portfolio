7b6b5ade4d35957be719a1f7bb56daf2ed84b74e
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-09 22:50:12 -0700

    added transition property with transform delay

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+/*$(".imageEffect").hover(function hoverOver() {
+    $(".imageEffect").animate({height: "165px", width: "220px"}, "slow")
+}, function hoverLeave() {
+    $(".imageEffect").animate({height: "150px", width: "200px"}, "slow")
+})*/
